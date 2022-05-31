7caf043ed6ae18a6c7cf0b0cf5810107a14cc9b7
hokyunrhee <hokyun.rhee@gmail.com>
2022-05-31 21:40:31 +0900

    feat: add Task

--- frontend/src/todo/components/task/task.tsx
+++ frontend/src/todo/components/task/task.tsx
@@ -2,13 +2,16 @@ interface TaskProps {
   id: string
   title: string
   completed: boolean
+  onCheck: (id: string) => void
+  onDelete: (id: string) => void
 }
 
-export const Task = ({ id, title, completed }: TaskProps) => {
+export const Task = ({ id, title, completed, onCheck, onDelete }: TaskProps) => {
   return (
     <div>
       <div>{title}</div>
-      <input type="checkbox" checked={completed} />
+      <input type="checkbox" checked={completed} onChange={() => onCheck(id)} />
+      <button onClick={() => onDelete(id)} />
     </div>
   )
 }
