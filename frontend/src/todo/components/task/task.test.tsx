4de7205b4e94ca88baf0e035a4996dfee01bff36
hokyunrhee <hokyun.rhee@gmail.com>
2022-05-31 19:12:20 +0900

    test: add test for Task

--- frontend/src/todo/components/task/task.test.tsx
+++ frontend/src/todo/components/task/task.test.tsx
@@ -1,16 +1,41 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 
 import { Task } from "./task"
+import { task } from "./__fixtures__/task"
 
 describe("Task", () => {
   it("renders with title and checkbox", () => {
-    const todoItem = { id: "1", title: "go to bed", completed: false }
-    render(<Task {...todoItem} />)
+    render(<Task {...task} />)
 
-    const title = screen.getByText(todoItem.title)
+    const title = screen.getByText(task.title)
     const checkbox = screen.getByRole("checkbox")
 
     expect(title).toBeInTheDocument()
     expect(checkbox).not.toBeChecked()
+  })
+
+  it("clicks checkbox", () => {
+    const mockOnCheck = jest.fn()
+
+    render(<Task {...task} onCheck={mockOnCheck} />)
+
+    const checkbox = screen.getByRole("checkbox")
+    userEvent.click(checkbox)
+
+    expect(mockOnCheck).toBeCalledTimes(1)
+    expect(mockOnCheck).toBeCalledWith(task.id)
+  })
+
+  it("clicks delete button", () => {
+    const mockOnDelete = jest.fn()
+
+    render(<Task {...task} onDelete={mockOnDelete} />)
+
+    const deleteButton = screen.getByRole("button")
+    userEvent.click(deleteButton)
+
+    expect(mockOnDelete).toBeCalledTimes(1)
+    expect(mockOnDelete).toBeCalledWith(task.id)
   })
 })
