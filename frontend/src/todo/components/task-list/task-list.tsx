f4a7c90178e50f631e0e948cad9be9f455073407
hokyunrhee <hokyun.rhee@gmail.com>
2022-06-02 21:45:05 +0900

    feat: add TaskList

--- frontend/src/todo/components/task-list/task-list.tsx
+++ frontend/src/todo/components/task-list/task-list.tsx
@@ -1,1 +1,12 @@
-import styles from "task-list.module.scss"
+import { Task } from "@/todo/components/task"
+import { TaskListProps } from "@/todo/domain/interface"
+
+export const TaskList = ({ tasks, onCheck, onDelete, onUpdate }: TaskListProps) => {
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <Task key={task.id} {...task} onCheck={onCheck} onDelete={onDelete} onUpdate={onUpdate} />
+      ))}
+    </ul>
+  )
+}
