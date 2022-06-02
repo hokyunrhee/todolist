206aa1d7049a75b33c417998ad51b7610773c8d3
hokyunrhee <hokyun.rhee@gmail.com>
2022-06-02 21:40:07 +0900

    test: refactor test for TaskList

--- frontend/src/todo/components/task-list/task-list.test.tsx
+++ frontend/src/todo/components/task-list/task-list.test.tsx
@@ -3,9 +3,17 @@ import { render, screen } from "@testing-library/react"
 import { TaskList } from "./task-list"
 import { tasks } from "./__fixtures__/tasks"
 
-const setup = () => render(<TaskList tasks={tasks} />)
+const mockOnCheck = jest.fn()
+const mockOnDelete = jest.fn()
+const mockOnUpdate = jest.fn()
+const setup = () =>
+  render(<TaskList tasks={tasks} onCheck={mockOnCheck} onDelete={mockOnDelete} onUpdate={mockOnUpdate} />)
 
 describe("TaskList", () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+
   it("renders list of items with provided tasks", () => {
     setup()
 
