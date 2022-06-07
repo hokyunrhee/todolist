c3735caae1284fb9c9859d3db1cbd208ef9d0875
hokyunrhee <hokyun.rhee@gmail.com>
2022-06-08 00:50:51 +0900

    feat: add Footer

--- frontend/src/todo/components/footer/footer.tsx
+++ frontend/src/todo/components/footer/footer.tsx
@@ -1,1 +1,24 @@
-import styles from "footer.module.scss"
+import { useRouter } from "next/router"
+
+interface FooterProps {
+  onClearCompleted: () => void
+  taskCount: number
+}
+
+export const Footer = ({ onClearCompleted, taskCount }: FooterProps) => {
+  const router = useRouter()
+
+  const handleClearCompleted = () => {
+    onClearCompleted()
+  }
+
+  return (
+    <div>
+      <div>{taskCount}</div>
+      <button onClick={() => router.push("/")}>all</button>
+      <button onClick={() => router.push("/active")}>active</button>
+      <button onClick={() => router.push("/completed")}>completed</button>
+      <button onClick={handleClearCompleted}>clear completed</button>
+    </div>
+  )
+}
