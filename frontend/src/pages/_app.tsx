7d13ed2333dad8159e2209006f940530ea46181b
hokyunrhee <hokyun.rhee@gmail.com>
2022-06-12 17:24:55 +0900

    build: add MSW

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from "next/app"
 import "@/styles/globals.css"
 
+if (process.env.NODE_ENV === "development") {
+  if (typeof window !== "undefined") {
+    const { worker } = require("@/mocks/browser")
+    worker.start()
+  }
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
